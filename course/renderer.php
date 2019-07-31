1c1287e657cfbf1a9fe40a98586e1e91fcfc9c0e
Sagar Ghimire <sagarghimire@catalyst-au.net>
2019-08-01 09:52:12 +1000

    MDL-60916 core_course: Replace course search with global search

--- course/renderer.php
+++ course/renderer.php
@@ -372,12 +372,21 @@ class core_course_renderer extends plugin_renderer_base {
                 $inputsize = 30;
         }
 
+        $globalsearch = get_config('core', 'enableglobalsearch');
+        $searchurl = new moodle_url('/course/search.php');
+        $name = 'search';
+        if ($globalsearch) {
+            $searchurl = new moodle_url('/search/index.php');
+            $name = 'q';
+        }
+
         $data = (object) [
-                'searchurl' => (new moodle_url('/course/search.php'))->out(false),
+                'searchurl' => ($searchurl)->out(false),
                 'id' => $formid,
                 'inputid' => $inputid,
                 'inputsize' => $inputsize,
-                'value' => $value
+                'value' => $value,
+                'name'  => $name
         ];
         if ($format != 'navbar') {
             $helpicon = new \help_icon('coursesearch', 'core');
